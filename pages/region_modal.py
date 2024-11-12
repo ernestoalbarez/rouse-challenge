8477717d0d0e60c7e4785a64416f2c5c9d0bea60
Ernesto <54443164+ernestoalbarez@users.noreply.github.com>
2024-11-12 19:30:06 -0300

    Update page methods

--- pages/region_modal.py
+++ pages/region_modal.py
@@ -1,10 +1,35 @@
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from locators import RegionModalLocators
 
 class RegionModal:
+    """
+    Page Object for Region Modal. Handles interactions with the region selection modal.
+    """
     def __init__(self, driver):
         self.driver = driver
 
     def get_elements_count(self):
+        """
+        Return the number of elements displayed in the region modal.
+        """
         elements = self.driver.find_elements(*RegionModalLocators.REGION_MODAL_ELEMENTS)
         return len(elements)
+
+    def search_language(self, language):
+        """
+        Search for a specific language in the region modal's search input.
+        """
+        language_input = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(RegionModalLocators.REGION_SEARCH)
+        )
+        language_input.clear()
+        language_input.send_keys(language)
+
+    def select_language(self, language):
+        """
+        Select a language from the region modal by clicking on the corresponding region.
+        """
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(RegionModalLocators.REGION[language])
+        ).click()
--- pages/search_page.py
+++ pages/search_page.py
@@ -2,18 +2,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from locators import SearchPageLocators
-
+import config
 
 class SearchPage:
+    """
+    Page Object for Search Page. Handles interactions with the DuckDuckGo search page.
+    """
     def __init__(self, driver):
         self.driver = driver
-        self.url = "https://start.duckduckgo.com/"
+        self.url = config.BASE_URL
 
     def open(self):
+        """
+        Open the DuckDuckGo search page.
+        """
         self.driver.get(self.url)
 
     def search(self, query):
+        """
+        Perform a search on DuckDuckGo with the given query.
+        """
         search_field = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(SearchPageLocators.SEARCH_INPUT)
         )
@@ -22,6 +32,9 @@ class SearchPage:
         search_field.send_keys(Keys.RETURN)
 
     def get_result_titles(self):
+        """
+        Return the list of result titles from the search results.
+        """
         articles = WebDriverWait(self.driver, 10).until(
             EC.presence_of_all_elements_located(SearchPageLocators.RESULT_TITLES)
         )
@@ -29,6 +42,29 @@ class SearchPage:
         return spans
 
     def click_region_button(self):
+        """
+        Click the Region button to open the region selection modal.
+        """
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(SearchPageLocators.REGION_BUTTON)
         ).click()
+
+    def click_more_results_button(self):
+        """
+        Click the 'More Results' button to load more search results.
+        """
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(SearchPageLocators.MORE_RESULTS_BUTTON)
+        ).click()
+
+    def is_article_present(self, language):
+        """
+        Check if an article in the selected language is present.
+        """
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable(SearchPageLocators.WIKI_PAGE[language])
+            )
+            return True
+        except TimeoutException:
+            return False
